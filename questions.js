3fe29821358e2adde4ddeedbeeb6003fe2511688
Anna Chernova <annanvks@gmail.com>
2023-12-22 19:37:39 +0000

    added questions with answers to questions.js file

---
+++ questions.js
@@ -0,0 +1,67 @@
+//questions - an array of objects
+//Each object has a question, all answers (an array of answers), and a correctAnswer (it is a number - the position of the right answer in an answers array).
+//
+
+let questions = [
+  {
+    question: "What is JavaScript primarily used for?",
+    answers: [
+      "Styling web pages",
+      "Enhancing interactivity on websites",
+      "Creating databases",
+      "Managing server-side operations",
+    ],
+    correctAnswer: 1,
+  },
+  {
+    question: "Which keyword is used to declare a variable in JavaScript?",
+    answers: ["var", "let", "const", "both B and C"],
+    correctAnswer: 3,
+  },
+  {
+    question: "How can you comment a single line of code in JavaScript?",
+    answers: [
+      "<!--This is a comment-->",
+      "// This is a comment",
+      "/* This is a comment */",
+      "# This is a comment",
+    ],
+    correctAnswer: 1,
+  },
+  {
+    question: "What is the purpose of the querySelector method in JavaScript?",
+    answers: [
+      "To select and manipulate HTML elements",
+      "To query a database",
+      "To perform mathematical calculations",
+      "To create a new variable",
+    ],
+    correctAnswer: 0,
+  },
+  {
+    question:
+      "Which method is used to add a new element at the end of an array in JavaScript?",
+    answers: ["push()", "endAdd()", "put()", "insert()"],
+    correctAnswer: 0,
+  },
+  {
+    question: "What does the === operator check in JavaScript?",
+    options: [
+      "Value and type equality",
+      "Value equality",
+      "Type equality",
+      "Reference equality",
+    ],
+    correctAnswer: 0,
+  },
+  {
+    question: "What is the purpose of the return statement in a function?",
+    answers: [
+      "To print a value to the console",
+      "To exit the function and return to the calling code",
+      "To define a new variable",
+      "To declare a function",
+    ],
+    correctAnswer: 1,
+  },
+];
