9c71d4e3f17dbc58edc6d668e7d9bd0128c7d764
Anna Chernova <annanvks@gmail.com>
2024-01-02 18:25:15 +0000

    add highscore ordering

--- scores.js
+++ scores.js
@@ -8,9 +8,16 @@ clear.addEventListener("click", function () {
 const highscores = JSON.parse(localStorage.getItem("highscores")) || [];
 
 function printHighscores() {
+  highscores.sort(function (a, b) {
+    if (a.score > b.score) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
   for (let i = 0; i < highscores.length; i++) {
     const li = document.createElement("li");
-    li.textContent = highscores[i].name + " - " + highscores[i].score;
+    li.textContent = highscores[i].name + " : " + highscores[i].score;
     document.getElementById("highscores").appendChild(li);
   }
 }
