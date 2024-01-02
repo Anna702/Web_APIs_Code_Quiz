abf8544d0aa2adc294f35d406745b2c8985bdb7d
Anna Chernova <annanvks@gmail.com>
2024-01-02 13:09:21 +0000

    add start button hide

--- script.js
+++ script.js
@@ -4,6 +4,8 @@ const startBtn = document.getElementById("start");
 
 startBtn.addEventListener("click", function () {
   //btn hide
+  startBtn.style.display = "none";
   //timer starts
+
   //first q appears
 });
