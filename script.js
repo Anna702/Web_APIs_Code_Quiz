6b2bbe15fd382d98fbbb0c7cb5c42f02c6f82c20
Anna Chernova <annanvks@gmail.com>
2024-01-02 13:25:57 +0000

    add timer that counts time from 75 to 0

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
 //A start button: when clicked a timer starts and the first question appears.
 
 const startBtn = document.getElementById("start");
+const timeCount = document.getElementById("time");
+let timeLeft = 75;
 
 startBtn.addEventListener("click", function () {
   //btn hide
   startBtn.style.display = "none";
   //timer starts
+  const timerInterval = setInterval(function () {
+    timeLeft--;
+    timeCount.textContent = timeLeft;
+    if (timeLeft <= 0) {
+      // Stops execution of action at set interval
+      clearInterval(timerInterval);
+    }
+  }, 1000);
 
   //first q appears
 });
